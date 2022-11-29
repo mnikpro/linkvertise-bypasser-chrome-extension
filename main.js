2117143314971ade33227f7d6c3105746b1d49e2
mnikpro <97117758+mnikpro@users.noreply.github.com>
2022-11-29 22:02:35 +0200

    Now it use code instead of a server

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-/*
-do a http GET request in 
-"https://vacant-curtly-composure.herokuapp.com/bypass2?url=" + current url"
-*/
-
-
-
-    var url = "https://vacant-curtly-composure.herokuapp.com/bypass2?url=" + window.location.href;
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.send();
-    
-    //redirect to the new url
-    xhr.onload = () => location.replace("https://google.com/");
-
-    console.log(xhr.response);
-    console.log(xhr.response);
-    console.log(xhr.response);
-    console.log(xhr.response);
-    console.log(xhr.response);
-
+if (window.location.href.startsWith('https://linkvertise')) {
+  //redirect to google.com
+    window.location.href = 'https://google.com'; // Test redirect capability
+}
